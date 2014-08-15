844bd634c4adaeca3838ca64f96a8b055b9840f9
Wang Jiadong <wangjiadong1993@gmail.com>
2014-08-15 10:12:30 +0800

    modification #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/modimp/faculty', to: "modimp#faculty"
+  root 'modimp#faculty'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

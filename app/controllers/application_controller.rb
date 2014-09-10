7565964ec0f1e84a7b21a45c4b9d4aba83c39962
Wang Jiadong <wangjiadong1993@gmail.com>
2014-09-10 15:56:36 +0800

    cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 # require 'mechanize'
 class ApplicationController < ActionController::Base
+	before_filter :cors
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+ 	private
+	def cors
+		headers["Access-Control-Allow-Origin"]  = "*"
+		headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+		headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+	      	head(:ok) if request.request_method == "OPTIONS"
+	end
 end
